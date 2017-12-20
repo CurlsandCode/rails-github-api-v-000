c8a57919f775448ad70026a10b02d61651267de6
Monika Williams <monika.m.williams@gmail.com>
2017-12-20 10:13:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
-    unless logged_in?
       github_oauth_url = "https://github.com/login/oauth/authorize"
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
       client_id = ENV['GITHUB_CLIENT_ID']
       github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
-      redirect_to github_url
+      redirect_to github_url unless logged_in?
     end
   end
 
